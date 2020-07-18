8cfc5a72c6e1a806799f211e02654891b502a311
Azucena Herrera <azucenaphx@gmail.com>
2020-07-18 08:18:57 -0700

    created files for app

---
+++ employee-directory/src/controller/API.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/";
+//const APIKEY = "&apikey=trilogy";
+
+export default {
+  search: async function() {
+    let response = 
+      await axios.get(BASEURL )
+      .then(x => x)
+      .catch(err => console.log("error: ",err));
+    console.log("API.search() called",response); 
+    return response;
+  }
+};
