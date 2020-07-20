6de3c592dc5696c7cc1a82acbb55210842c94225
Azucena Herrera <azucenaphx@gmail.com>
2020-07-20 08:29:27 -0700

    final changes

--- employee-directory/src/components/EmployeeDetail.js
+++ employee-directory/src/components/EmployeeDetail.js
@@ -3,7 +3,7 @@ import React from "react";
 function EmployeeDetail(props) {
   return (
     <div className="text-center">
-      <ul className="list-group list-group-horizontal-sm list-group-primary">
+      <ul className="list-group list-group-flush list-group-horizontal-sm">
       <li className="list-group-item"><img alt={props.name} className="img-fluid" src={props.picture} style={{ margin: "0 auto" }} /></li>
       <li className="list-group-item"> {props.name.first} {props.name.last}</li>
       <li className="list-group-item"> {props.phone}</li>
